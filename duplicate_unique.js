398c6fa29a36823771417ba274974d3af54467d7
hemanth <lshemanth777.com>
2024-03-27 15:57:48 +0530

    first program

---
+++ duplicate_unique.js
@@ -0,0 +1,21 @@
+//function to find the dupicate in an array
+
+const arr = [1, 2, 4, 3, 5, 3, 5, 5, 6];
+
+function duplicate(arr) {
+  return (duplicateValues = arr.filter((ele, i, arr) => {
+    return arr.indexOf(ele) !== i;
+  }));
+}
+const Answer = duplicate(arr);
+console.log(Answer);
+
+// Function to find the unique elements of the array
+
+function unique(arr) {
+  return (uniqueValues = arr.filter((ele, i, arr) => {
+    return arr.indexOf(ele) === i;
+  }));
+}
+const UniqueAns = unique(arr);
+console.log(UniqueAns);
